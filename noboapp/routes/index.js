89c6ac19b0cfa48cb4ca5a66767666728094411b
Bellakhal Mohamed <mohamed.bellakhal@supcom.tn>
2020-10-08 12:12:00 +0100

    formcreated

--- noboapp/routes/index.js
+++ noboapp/routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('test');
   res.render('index', { title: 'Express' });
 });
 
-
+/* POST form variables */ 
+router.post('/', function(req, res){
+  console.log(req.body.string);
+  var chaine=req.body.string
+  var num=req.body.number 
+  res.send("recieved your request!");
+});
 /* GET SECOND PAGE */
-router.get('/hello', function(req, res){
+router.get('/Annexe2', function(req, res){
    res.send("Hello World!");
 });
 
