0e6516a7db662b19788a3ec423396cea43cff6d6
Bellakhal Mohamed <mohamed.bellakhal@supcom.tn>
2020-10-08 12:35:35 +0100

    formpresque

--- noboapp/routes/index.js
+++ noboapp/routes/index.js
@@ -4,16 +4,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('test');
   res.render('index', { title: 'Express' });
 });
 
 /* POST form variables */ 
 router.post('/', function(req, res){
   console.log(req.body.string);
-  var chaine=req.body.string
-  var num=req.body.number 
-  res.send("recieved your request!");
+  var chaine=String(req.body.string);
+  var num=parseInt(req.body.number); 
+  if (num>=0 && num<=20 && chaine.length>0 )
+  {
+    var a=chaine.len/num
+    console.log(chaine.len)
+    console.log(num)
+    console.log(a)
+    res.send(String(a));
+  } 
+  else 
+  {
+    res.send("recieved your request!");
+  }
 });
 /* GET SECOND PAGE */
 router.get('/Annexe2', function(req, res){
